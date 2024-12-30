693edf5d09a8e139759bfbbcb60d8fd400d9deae
om <awsdevopsom@gmail.com>
2024-12-30 22:38:45 +0530

    om

--- main.tf
+++ main.tf
@@ -3,6 +3,11 @@ module "vpc" {
    vpc_cidr_block = var.vpc_cidr_block  
    env = var.env
    tags = var.tags
+   public_subnets = var.public_subnets
+   app_subnets = var.app_subnets
+   db_subnets = var.db_subnets
+   web_subnets = var.web_subnets
+   azs = var.azs
 }
 
 
