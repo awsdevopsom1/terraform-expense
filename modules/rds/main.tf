f1f6cfdf223ab5f05a16cce2038d39b0fdec366c
om <awsdevopsom@gmail.com>
2025-01-01 22:40:04 +0530

    om

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -48,7 +48,7 @@ resource "aws_db_instance" "main" {
   storage_type         = "gp3"
   tags                 = merge(var.tags, { Name = "${var.env}-mysql-rds"})
   db_subnet_group_name = aws_db_subnet_group.main.name 
-  vpc_security_group_ids = [aws.aws_security_group.main.id]
+  vpc_security_group_ids = [aws_security_group.main.id]
 
 }
 
